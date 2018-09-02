2807182e983066254d4648ee9d53f9840287b81e
winghoi <winghoi@192.168.1.91>
2018-09-02 17:38:35 +0800

    first commit

---
+++ lesosn01_func_and_var.py
@@ -0,0 +1,10 @@
+'''
+variable and functions
+'''
+def question():
+    my_answer = input("What is your name? ")
+    print("Hello " + my_answer + ", how are you?")
+
+if __name__ == '__main__':
+    question()
+    question()
